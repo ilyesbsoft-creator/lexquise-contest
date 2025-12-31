03a74e6045d3f4c86bd03f420ea6957ebe4cedd2
ilyesbsoft-creator <ilyes.bsoft@gmail.com>
2025-12-31 10:21:43 -0800

    update sa7b 001

--- backend/index.js
+++ backend/index.js
@@ -15,6 +15,7 @@ const app = express();
 // =======================
 //  CORS FIX
 // =======================
+
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "https://lexquise-contest.vercel.app");
   res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
